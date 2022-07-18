89e46029d33d5c8cad384b5c1a6ed03bcfc65bcd
Oka Wibawa <okaa.wibawa@gmail.com>
2022-07-18 19:19:32 +0800

    fixes seo

--- components/Head.jsx
+++ components/Head.jsx
@@ -6,37 +6,31 @@ const HeadSeo = () => {
     <Head>
       {/* <!-- Primary Meta Tags --> */}
       <title>Sanatras</title>
-      <meta name="title" content="Meta Tags — Preview, Edit and Generate" />
+      <meta name="title" content="Sanatras — Wikipedia Upacara Yadnya" />
       <meta
         name="description"
-        content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+        content="Sanatras merupakan sebuah situs yang menyediakan informasi mengenai Upacara Yadnya di Bali beserta properti-properti yang digunakan."
       />
 
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://metatags.io/" />
-      <meta property="og:title" content="Meta Tags — Preview, Edit and Generate" />
+      <meta property="og:url" content="https://sanatras.vercel.app" />
+      <meta property="og:title" content="Sanatras — Wikipedia Upacara Yadnya" />
       <meta
         property="og:description"
-        content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+        content="Sanatras merupakan sebuah situs yang menyediakan informasi mengenai Upacara Yadnya di Bali beserta properti-properti yang digunakan."
       />
-      <meta
-        property="og:image"
-        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-      />
+      <meta property="og:image" content="https://i.ibb.co/R2T37Zy/hero.png" />
 
       {/* <!-- Twitter --> */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:url" content="https://metatags.io/" />
-      <meta property="twitter:title" content="Meta Tags — Preview, Edit and Generate" />
+      <meta property="twitter:url" content="https://sanatras.vercel.app" />
+      <meta property="twitter:title" content="Sanatras — Wikipedia Upacara Yadnya" />
       <meta
         property="twitter:description"
-        content="With Meta Tags you can edit and experiment with your content then preview how your webpage will look on Google, Facebook, Twitter and more!"
+        content="Sanatras merupakan sebuah situs yang menyediakan informasi mengenai Upacara Yadnya di Bali beserta properti-properti yang digunakan."
       />
-      <meta
-        property="twitter:image"
-        content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png"
-      ></meta>
+      <meta property="twitter:image" content="https://i.ibb.co/R2T37Zy/hero.png"></meta>
     </Head>
   );
 };
